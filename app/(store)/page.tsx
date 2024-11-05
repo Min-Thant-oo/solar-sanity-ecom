3ec5f4fe0a6e5deb03835221f2bed455ed10e98c
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-11-05 15:41:26 -0600

    Products query implemented

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,11 +1,19 @@
+import ProductsView from "@/components/ProductsView";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+  const categories = await getAllCategories();
+
+  // console.log(crypto.randomUUID().slice(0,5) + `>>> Rerendered the home page with cache with ${products.length} products and ${categories.length} categories`)
+
   return (
     <div>
       <h1>Hello</h1>
-      <Button>Click me</Button>
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} />
+      </div>
     </div>
   );
 }
